9e2a0dfb6b421b2368486e2d522521a4fb172b2e
Fernando Castro <fca.apps@gmail.com>
2017-04-28 23:15:11 -0300

    Criado processo de Alterar Senha

--- controleonibus/accounts/urls.py
+++ controleonibus/accounts/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url, include
 
 from controleonibus.accounts.views import register
 
+from controleonibus.accounts import views
+
 from django.contrib.auth.views import login
 
 from django.contrib.auth.views import logout
@@ -11,5 +13,7 @@ urlpatterns = [
     url(r'^entrar/$', login, {'template_name': 'accounts/login.html'},  name="login"),
     url(r'^sair/$', logout, {'next_page': 'accounts:login'}, name="logout"),
     url(r'^registre-se/$', register, name="register"),
+    url(r'^editar-senha/$', views.edit_password, name="edit_password"),
+    url(r'^aviso-senha-alterada/$', views.aviso_senha_alterada, name="senha_alterada"),
 ]
 
--- controleonibus/accounts/views.py
+++ controleonibus/accounts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.conf import settings
 from django.contrib.auth import authenticate, login
 from .forms import RegisterForm
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     template_name = 'accounts/register.html'
@@ -21,3 +22,21 @@ def register(request):
         'form': form
     }
     return render(request, template_name, context)
+
+@login_required
+def edit_password(request):
+    template_name = 'accounts/edit_password.html'
+    context = {}
+    if request.method=="POST":
+        form = PasswordChangeForm(data=request.POST, user=request.user)
+        if form.is_valid():
+            form.save()
+            context['success'] = True
+            return redirect('accounts:senha_alterada')
+    else:
+        form = PasswordChangeForm(user=request.user)
+    context['form'] = form
+    return render(request, template_name, context)
+
+def aviso_senha_alterada(request):
+    return render(request, 'accounts/aviso_senha_alterada.html')
